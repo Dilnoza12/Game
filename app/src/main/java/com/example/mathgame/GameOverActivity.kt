3b143e0fdd6bb67b3b18911e5b7ff6a7899b7aee
Dilnoz12 <dzuldasbaevad@gmail.com>
2020-07-22 22:13:44 +0500

    add additional fun

--- app/src/main/java/com/example/mathgame/GameOverActivity.kt
+++ app/src/main/java/com/example/mathgame/GameOverActivity.kt
@@ -10,8 +10,8 @@ class GameOverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_over)
-        val result=intent.getStringExtra("result")
-        tvRight.text=result
+       var rightAnswersCount=intent.getIntExtra(Main2Activity.RIGHT_ANSWERS_COUNT,0)
+        tvRight.text="$rightAnswersCount/${Main2Activity.LEVEL_COUNT}"
         Restartbtn.setOnClickListener {
             val intent= Intent(this,MainActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
